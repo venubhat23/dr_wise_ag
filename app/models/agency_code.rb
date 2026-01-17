b02825c7e1422e73c51687802e876a44b2da0741
venu bhat <venubha147@gmail.com>
2026-01-17 12:42:59 +0530

    test

--- app/models/agency_code.rb
+++ app/models/agency_code.rb
@@ -11,7 +11,6 @@ class AgencyCode < ApplicationRecord
   validates :code, presence: true, uniqueness: { scope: [:company_name, :insurance_type] }
 
   # Custom validation to ensure company_name is from predefined list
-  validate :company_name_must_be_valid
 
   # Scopes for filtering
   scope :by_insurance_type, ->(type) { where(insurance_type: type) if type.present? }
@@ -30,12 +29,4 @@ class AgencyCode < ApplicationRecord
   end
 
   private
-
-  def company_name_must_be_valid
-    return if company_name.blank?
-
-    unless self.class.insurance_company_names.include?(company_name)
-      errors.add(:company_name, "must be a valid insurance company")
-    end
-  end
 end
